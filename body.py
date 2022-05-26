9b010fff387495c1c5a1b1f6c9adeff3ac6230b3
Anton-Tsypin <anton.tsypin.s@gmail.com>
2022-05-26 17:48:05 +0300

    some fixes

--- body.py
+++ body.py
@@ -2,78 +2,79 @@ import random
 from termcolor import colored
 
 class Body:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def attack(self, dodge=0.1):
-		r = random.random()
-		if r >= 0.95:
-			self.power_change(+round(self.power/20))
-			return 5 * self.power + random.randint(int(-self.power * 0.2), int(self.power * 0.2))
-		if r >= dodge:
-			return self.power + random.randint(int(-self.power * 0.2), int(self.power * 0.2))
-		elif r <= 0.1:
-			self.power_change(-round(self.power/20))
-		return 0
+    def attack(self, dodge=0.1): # определение нанесённого урона
+        r = random.random()
+        if r >= 0.95:
+            self.power_change(+round(self.power/20))
+            return 5 * self.power + random.randint(int(-self.power * 0.2), int(self.power * 0.2))
+        if r >= dodge:
+            return self.power + random.randint(int(-self.power * 0.2), int(self.power * 0.2))
+        elif r <= 0.1:
+            self.power_change(-round(self.power/20))
+        return 0
 
-	def get_damage(self, damage):
-		self.health -= damage
-		if self.health < 0:
-			self.health = 0
-		if self.health > self.maxhealth:
-			self.health = self.maxhealth
-					
-	def power_change(self, points):
-		self.power += points
-		if self.power < self.maxhealth//10:
-			self.power = self.maxhealth//10
+    def get_damage(self, damage): # получение урона
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
+        if self.health > self.maxhealth:
+            self.health = self.maxhealth
+                    
+    def power_change(self, points): # изменение силы
+        self.power += points
+        if self.power < self.maxhealth//10:
+            self.power = self.maxhealth//10
 
-	def is_dead(self):
-		return self.health <= 0
+    def is_dead(self):
+        return self.health <= 0
 
 
 class Enemy(Body):
-	def __init__(self, typ, name=''):
-		self.typ = typ
-		self.name = name
-		if self.typ == 'Skeleton':
-			x, y = 10, 15
-		elif self.typ == 'Goblin':
-			x, y = 15, 25
-		elif self.typ == 'Orc':
-			x, y = 30, 60
-		elif self.typ == 'Ogre':
-			x, y = 60, 90
-		elif self.typ == 'Dragon':
-			x, y = 100, 170
-		elif self.typ == 'Boss':
-			x, y = 200, 400
+    def __init__(self, typ, name=''):
+        self.typ = typ
+        self.name = name
+        types_of_enemies = {'skeleton': {'x': 10, 'y': 15}}
+        if self.typ == 'Skeleton':
+            x, y = 10, 15
+        elif self.typ == 'Goblin':
+            x, y = 15, 25
+        elif self.typ == 'Orc':
+            x, y = 30, 60
+        elif self.typ == 'Ogre':
+            x, y = 60, 90
+        elif self.typ == 'Dragon':
+            x, y = 100, 170
+        elif self.typ == 'Boss':
+            x, y = 200, 400
 
-		self.health = 10 * random.randint(x, y)
-		self.power = 2 * random.randint(x, y)
-		self.maxhealth = self.health
+        self.health = 10 * random.randint(x, y)
+        self.power = 2 * random.randint(x, y)
+        self.maxhealth = self.health
 
-	def death(self):
-		print(self.typ, self.name, colored('is defeated!', 'cyan'))
+    def death(self):
+        print(f"{self.typ} {self.name} {colored('is defeated!', 'cyan')}")
 
 
 class Player(Body):
-	def __init__(self, name=''):
-		self.name = name
-		self.health = 500
-		self.maxhealth = self.health
-		self.power = 100
-		
-		self.killed_enemies = 0
-		self.got_maxhealth = 0
-		self.got_power = 0
-		return
+    def __init__(self, name=''):
+        self.name = name
+        self.health = 500
+        self.maxhealth = self.health
+        self.power = 100
+        
+        self.killed_enemies = 0
+        self.got_maxhealth = 0
+        self.got_power = 0
+        return
 
-	def steal_stats(self, defeated_enemy):
-		self.maxhealth += defeated_enemy.maxhealth//3
-		self.get_damage(-2*defeated_enemy.maxhealth//3)
-		self.power_change(defeated_enemy.power//3)
-		
-		self.got_maxhealth += defeated_enemy.maxhealth//3
-		self.got_power += defeated_enemy.power//3
-		self.killed_enemies += 1
+    def steal_stats(self, defeated_enemy):
+        self.maxhealth += defeated_enemy.maxhealth//3
+        self.get_damage(-2*defeated_enemy.maxhealth//3)
+        self.power_change(defeated_enemy.power//3)
+        
+        self.got_maxhealth += defeated_enemy.maxhealth//3
+        self.got_power += defeated_enemy.power//3
+        self.killed_enemies += 1
--- main.py
+++ main.py
@@ -3,123 +3,125 @@ from termcolor import colored
 from body import Player, Enemy
 
 def gen_enemy_list(n, types=None):
-	if types == None:
-		types = ['Skeleton', 'Goblin', 'Orc', 'Ogre', 'Dragon']
-	names = ['Griffin', 'Subarashi', 'David', 'Vladushka', 'Zhek', 'Arkasha', 'Danya', 'Soke']
-	enemy_list = []
-	for i in range(n):
-		enemy_list += [Enemy(random.choice(types), random.choice(names))]
-	return enemy_list
+    if types == None:
+        types = ['Skeleton', 'Goblin', 'Orc', 'Ogre', 'Dragon']
+    names = ['Griffin', 'Subarashi', 'David', 'Vladushka', 'Zhek', 'Arkasha', 'Danya', 'Soke']
+    enemy_list = []
+    for i in range(n):
+        enemy_list += [Enemy(random.choice(types), random.choice(names))]
+    return enemy_list
 
 def print_screen(stats, actions):
-	print(*stats, '\n')
-	for i in actions:
-		print(*i)
+    print(stats, '\n')
+    for action in actions:
+        print(action)
 
 def main():
-	player = Player(input('Enter the name of your hero: '))
-	print('Your task is to defeat all monsters and stay alive, good luck!')
-	print('To attack press a, to block press d\n')
-	input('Print anything to start: ')
-	enemy_list = gen_enemy_list(5, types=['Skeleton', 'Goblin', 'Orc'])
-	enemy_list += gen_enemy_list(5, types=['Goblin', 'Orc', 'Ogre'])
-	enemy_list += gen_enemy_list(5, types=['Orc', 'Ogre', 'Dragon'])
-	actions = []
-	flag = False
-	time.sleep(1)
-	os.system('cls')
-	while enemy_list:
-		enemy = enemy_list[0]
-		stats = (enemy.typ, enemy.name, 'have', colored(str(enemy.health), 'red') + '/' + colored(str(enemy.maxhealth), 'red'), 'health and', colored(enemy.power, 'yellow'),
-				 'power, \nYou have', colored(str(player.health), 'green') + '/' + colored(str(player.maxhealth), 'green'), 'health and', colored(player.power, 'yellow'), 'power')
-		print_screen(stats, actions)
-		if len(actions) > 20:
-			actions.pop(0)
+    os.system('cls')
+    print('Your task is to defeat all monsters and stay alive, good luck!')
+    print('To attack press a, to defend press d\n')
+    player = Player(input('Enter your name, hero: '))
+    enemy_list = gen_enemy_list(5, types=['Skeleton', 'Goblin', 'Orc'])
+    enemy_list += gen_enemy_list(5, types=['Goblin', 'Orc', 'Ogre'])
+    enemy_list += gen_enemy_list(5, types=['Orc', 'Ogre', 'Dragon'])
+    actions = []
+    boss_flag = True
+    time.sleep(1)
+    os.system('cls')
+    while enemy_list:
+        enemy = enemy_list[0]
+        en_hl, en_mhl, en_pw = colored(str(enemy.health), 'red'),  colored(str(enemy.maxhealth), 'red'), colored(enemy.power, 'yellow')
+        pl_hl, pl_mhl, pl_pw = colored(str(player.health), 'green'), colored(str(player.maxhealth), 'green'), colored(player.power, 'yellow')
+        stats = f"{enemy.typ} {enemy.name} have {en_hl}/{en_mhl} health and {en_pw} power \nYou have {pl_hl}/{pl_mhl} health and {pl_pw} power"
+        print_screen(stats, actions)
+        if len(actions) > 16:
+            actions.pop(0)
 
-		if player.is_dead():
-			print(colored('Death.', 'red'))
-			time.sleep(2)
-			os.system('cls')
-			print(colored('\n\n\tYou died', 'red'))
-			break
-		
-		if enemy.is_dead():
-			enemy.death()
-			enemy_list.pop(0)
-			
-			player.steal_stats(enemy)
-			
-			time.sleep(2)
-			actions = []
-			if len(enemy_list) != 0:
-				os.system('cls')
-			else:
-				if flag == False:
-					flag == True
-					enemy = Enemy('Boss', 'Mirror of ' + player.name)
-					enemy_list += [enemy]
-					enemy.health, enemy.maxhealth, enemy.power = player.health, player.maxhealth, player.power
-					os.system('cls')
-			continue
-		
-		action = input('Your action: ')
-		if action == 'a' or action == 'attack':
-			player_damage = player.attack()
-			enemy.get_damage(player_damage)
-			actions += [['You dealt', colored(player_damage, 'green'), 'damage,']]
-			
-		elif action == 'd' or action == 'defense':
-			player_damage = int(2 * (2*player.attack()+enemy.attack())/3)
-			player.get_damage(-player_damage)
-			player.power_change(-round(player.power/100))
-			actions += [['You restored', colored(player_damage, 'cyan'), 'health,']]
-			
-		elif action == 'cheat':
-			actions += [(colored('Cheater', 'yellow'), '')]
-			player.maxhealth += 10000
-			player.health += 10000
-			player.power += 1000
-			os.system('cls')
-			continue
+        if player.is_dead():
+            print(colored('Death.', 'red'))
+            time.sleep(2)
+            os.system('cls')
+            print(colored('\n\n\tYou died', 'red'))
+            break
+        
+        if enemy.is_dead():
+            enemy.death()
+            enemy_list.pop(0)
+            
+            player.steal_stats(enemy)
+            
+            time.sleep(2)
+            os.system('cls')
+            actions = []
+            if not enemy_list:
+                if boss_flag:
+                    boss_flag = False
+                    enemy = Enemy('Boss', f"Mirror of {player.name}")
+                    enemy_list += [enemy]
+                    enemy.health, enemy.maxhealth, enemy.power = player.health, player.maxhealth, player.power
+            continue
+        
+        action = input('Your action: ')
+        if action in ['a', 'attack']:
+            player_damage = player.attack()
+            enemy.get_damage(player_damage)
+            actions += [f"You dealt {colored(player_damage, 'green')} damage, "]
+            
+        elif action in ['d', 'defense']:
+            player_damage = int(2 * (2*player.attack()+enemy.attack())/3)
+            player.get_damage(-player_damage)
+            player.power_change(-round(player.power/100))
+            actions += [f"You restored {colored(player_damage, 'cyan')} health, "]
+            
+        elif action == 'cheat':
+            actions += [colored('Cheater', 'yellow')]
+            player.maxhealth += 10000
+            player.health += 10000
+            player.power += 1000
+            os.system('cls')
+            continue
 
-		elif action == 'death':
-			player.health = 0
-			os.system('cls')
-			continue
+        elif action == 'death':
+            player.health = 0
+            os.system('cls')
+            continue
 
-		elif action == 'win':
-			break
-		
-		elif action == 's' or action == 'stat':
-			actions += [(colored('(Killed enemies ' + str(player.killed_enemies) + ', stolen ' + str(player.got_maxhealth) +
-					' health and ' + str(player.get_power) + ' power)', 'magenta'), '')]
-			os.system('cls')
-			continue
-		
-		else:
-			actions += [(colored('Some nonsense', 'white'), '')]
-			os.system('cls')
-			continue
+        elif action == 'win':
+            break
+        
+        elif action in ['s', 'stat']:
+            actions += [colored(f"Killed enemies: {str(player.killed_enemies)}, stolen {str(player.got_maxhealth)} health and {str(player.got_power)} power", 'magenta')]
+            os.system('cls')
+            continue
 
-		if enemy.health > 0 and (enemy.health/enemy.maxhealth) <= 0.5 and random.random() <= 0.5:
-			enemy_damage = int(2 * (player.attack() + 2*enemy.attack())/3)
-			enemy.power_change(-round(enemy.power/100))
-			enemy.get_damage(-enemy_damage)
-			actions[-1] += ['enemy restored', colored(enemy_damage, 'cyan'), 'health.']
-		else:
-			enemy_damage = enemy.attack()
-			player.get_damage(enemy_damage)
-			actions[-1] += ['enemy deal you', colored(enemy_damage, 'red'), 'damage.']
-			
-		os.system('cls')
-		
-	if not player.is_dead():
-		time.sleep(1)
-		os.system('cls')
-		print(colored('\n\n\tYou win!', 'cyan'))
-	time.sleep(4)
-	return None
+        elif action in ['exit', 'quit']:
+            os.system('cls')
+            return False
+        
+        else:
+            actions += ["Some nonsense"]
+            os.system('cls')
+            continue
 
-while True:
-	os.system('cls')
-	main()
+        if enemy.health > 0 and (enemy.health/enemy.maxhealth) <= 0.5 and random.random() <= 0.5:
+            enemy_damage = int(2 * (player.attack() + 2*enemy.attack())/3)
+            enemy.power_change(-round(enemy.power/100))
+            enemy.get_damage(-enemy_damage)
+            actions[-1] += f"enemy restored {colored(enemy_damage, 'cyan')} health."
+            
+        else:
+            enemy_damage = enemy.attack()
+            player.get_damage(enemy_damage)
+            actions[-1] += f"enemy deal you {colored(enemy_damage, 'red')} damage."
+            
+        os.system('cls')
+        
+    if not (enemy_list or player.is_dead()):
+        time.sleep(1)
+        os.system('cls')
+        print(colored('\n\n\tYou win!', 'cyan'))
+    time.sleep(4)
+    return True
+
+while main():
+    os.system('cls')
