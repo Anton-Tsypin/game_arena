e001cd90a60b63cc750fc37b8b0e308bb667111b
Anton-Tsypin <anton.tsypin.s@gmail.com>
2022-05-28 14:58:07 +0300

    transferring actions to a separate class, transforming the main function into the game class

---
+++ actions.py
@@ -0,0 +1,88 @@
+from termcolor import colored
+import pickle, os
+
+class Action:
+    def __init__(self, action):
+        self.action = action
+
+    def do(self, game):
+        message = ['Some nonsense']
+        if self.action == '':
+            message = ['Some nonsense']
+
+        elif self.action in ['a', 'attack', 'd', 'defense']:
+            if self.action in ['a', 'attack']:
+                game.player_damage = game.player.attack(game.enemy)
+                game.enemy.get_damage(game.player_damage)
+                message = [f"You dealt {colored(game.player_damage, 'green')} damage, "]
+
+            elif self.action in ['d', 'defense']:
+                game.player_heal = game.player.healing(game.enemy)
+                game.player.get_healing(game.player_heal)
+                game.player.power_change(-round(game.player.power/100))
+                message = [f"You restored {colored(game.player_heal, 'cyan')} health, "]
+
+            message[0] += game.enemy.action(game.player)
+
+        elif self.action == 'cheat':
+            game.player.maxhealth += 10000
+            game.player.health += 10000
+            game.player.power += 1000
+            message = [colored('Cheater', 'yellow')]
+
+        elif self.action == 'death':
+            message = [(colored('Death.', 'red'))]
+            game.player.health = 0
+
+        elif self.action == 'win':
+            message = ["Победа!"]
+            game.enemy_list = []
+            game.fight_run_flag = False
+        
+        elif self.action in ['s', 'stat']:
+            message = [colored(f"Killed enemies: {str(game.player.killed_enemies)}, stolen {str(game.player.got_maxhealth)} health and {str(game.player.got_power)} power", 'magenta')]
+
+        elif self.action in ['exit', 'quit', '& D:/Python/python.exe d:/Python/Game_arena/main.py']:
+            game.main_game_flag = False
+            game.fight_run_flag = False
+            message = "Вы вышли из игры"
+
+        elif self.action in ['new', 'new game']:
+            game.fight_run_flag = False
+
+        elif self.action.split()[0] == 'save':
+            try:
+                if not os.path.exists('saves'): os.makedirs('saves') 
+                save_name = "fast" if len(self.action.split()) == 1 else self.action.split()[1]
+                save_data = {'player' : game.player, 'enemy_list' : game.enemy_list, 'boss_flag' : game.boss_flag}
+                with open(f"saves/{save_name}.save", 'wb') as file:
+                    pickle.dump(save_data, file)
+                message = [colored(f'Сохранение "{save_name}" создано', 'yellow')]
+            except:
+                message = [colored("Не удалось сохранить", 'yellow')]
+
+        elif self.action.split()[0] == 'load':
+            try:
+                save_name = "fast" if len(self.action.split()) == 1 else self.action.split()[1]
+                with open(f"saves/{save_name}.save", 'rb') as file:
+                    save_data = pickle.load(file)
+                    game.player = save_data['player']
+                    game.enemy_list = save_data['enemy_list']
+                    game.boss_flag = save_data['boss_flag']
+                message = [colored(f'Сохранение "{save_name}" загружено', 'yellow')]
+            except:
+                message = [colored("Не удалось загрузить сохранение", 'yellow')]
+
+        elif self.action == 'saves':
+            saves = ", ".join(map(lambda name: f'"{name[0:-5]}"', os.listdir("saves/")))
+            message = [f"Список сохранений: {saves}"]
+
+        elif self.action.split()[0] in ['del', 'delete']:
+            try:
+                save_name = "fast" if len(self.action.split()) == 1 else self.action.split()[1]
+                os.remove(f"saves/{save_name}.save")
+                message = [colored(f'Сохранение "{save_name}" удалено', 'yellow')]
+            except:
+                message = [colored("Не удалось удалить сохранение", 'yellow')]
+
+        return message
--- main.py
+++ main.py
@@ -1,150 +1,85 @@
 import random, time, os
-import pickle
 from termcolor import colored
 from body import Player, Enemy
+from actions import Action
 
-def gen_enemy_list(n, types=None):
-    if types == None:
-        types = ['Skeleton', 'Goblin', 'Orc', 'Ogre', 'Dragon']
-    names = ['Griffin', 'Subarashi', 'David', 'Vladushka', 'Zhek', 'Arkasha', 'Danya', 'Soke']
-    enemy_list = []
-    for i in range(n):
-        enemy_list += [Enemy(random.choice(types), random.choice(names))]
-    return enemy_list
 
-def print_screen(stats, actions): # отрисовка текста на экране
-    os.system('cls')
-    print(stats, '\n')
-    for action in actions:
-        print(action)
+class Game:
+    def __init__(self):
+        self.player = Player()
+        self.fight_run_flag = True
+        self.main_game_flag = True
+        self.enemy = None
 
-def main():
-    print('Your task is to defeat all monsters and stay alive, good luck!')
-    print('To attack press a, to defend press d\n')
-    player = Player(input('Enter your name, hero: '))
-    enemy_list = gen_enemy_list(5, types=['Skeleton', 'Goblin', 'Orc'])
-    enemy_list += gen_enemy_list(5, types=['Goblin', 'Orc', 'Ogre'])
-    enemy_list += gen_enemy_list(5, types=['Orc', 'Ogre', 'Dragon'])
-    actions = []
-    boss_flag = True
-    time.sleep(1)
-    os.system('cls')
-    while enemy_list:
-        enemy = enemy_list[0]
-        en_hl, en_mhl, en_pw = colored(str(enemy.health), 'red'),  colored(str(enemy.maxhealth), 'red'), colored(enemy.power, 'yellow')
-        pl_hl, pl_mhl, pl_pw = colored(str(player.health), 'green'), colored(str(player.maxhealth), 'green'), colored(player.power, 'yellow')
-        stats = f"{enemy.typ} {enemy.name} have {en_hl}/{en_mhl} health and {en_pw} power \nYou have {pl_hl}/{pl_mhl} health and {pl_pw} power"
-        print_screen(stats, actions)
-        if len(actions) > 16:
-            actions.pop(0)
+    def gen_enemy_list(self, n, types=None):
+        if types == None:
+            types = ['Skeleton', 'Goblin', 'Orc', 'Ogre', 'Dragon']
+        names = ['Griffin', 'Subarashi', 'David', 'Vladushka', 'Zhek', 'Arkasha', 'Danya', 'Soke']
+        enemy_list = []
+        for i in range(n):
+            enemy_list += [Enemy(random.choice(types), random.choice(names))]
+        return enemy_list
 
-        if player.is_dead():
-            print(colored('Death.', 'red'))
-            time.sleep(2)
-            os.system('cls')
-            print(colored('\n\n\tYou died', 'red'))
-            break
-        
-        if enemy.is_dead():
-            enemy.death()
-            enemy_list.pop(0)
-            
-            player.steal_stats(enemy)
-            
-            time.sleep(2)
-            actions = []
-            if not enemy_list:
-                if boss_flag:
-                    boss_flag = False
-                    enemy = Enemy('Boss', f"Mirror of {player.name}")
-                    enemy_list += [enemy]
-                    enemy.health, enemy.maxhealth, enemy.power = player.health, player.maxhealth, player.power
-            continue
-        
-        action = input('Your action: ')
-        if action is '':
-            actions += ["Some nonsense"]
-
-        elif action in ['a', 'attack']:
-            player_damage = player.attack(enemy)
-            enemy.get_damage(player_damage)
-            actions += [f"You dealt {colored(player_damage, 'green')} damage, "]
-            
-        elif action in ['d', 'defense']:
-            player_heal = player.healing(enemy)
-            player.get_healing(player_heal)
-            player.power_change(-round(player.power/100))
-            actions += [f"You restored {colored(player_heal, 'cyan')} health, "]
-            
-        elif action == 'cheat':
-            actions += [colored('Cheater', 'yellow')]
-            player.maxhealth += 10000
-            player.health += 10000
-            player.power += 1000
-
-        elif action == 'death':
-            player.health = 0
-
-        elif action == 'win':
-            break
-        
-        elif action in ['s', 'stat']:
-            actions += [colored(f"Killed enemies: {str(player.killed_enemies)}, stolen {str(player.got_maxhealth)} health and {str(player.got_power)} power", 'magenta')]
-
-        elif action in ['exit', 'quit', '& D:/Python/python.exe d:/Python/Game_arena/main.py']:
-            return False
-
-        elif action in ['new', 'new game']:
-            return True
-        
-        elif action.split()[0] == 'save':
-            try:
-                if not os.path.exists('saves'): os.makedirs('saves') 
-                save_name = "fast" if len(action.split()) == 1 else action.split()[1]
-                save_data = {'player' : player, 'enemy_list' : enemy_list, 'boss_flag' : boss_flag}
-                with open(f"saves/{save_name}.save", 'wb') as file:
-                    pickle.dump(save_data, file)
-                actions += [colored(f'Сохранение "{save_name}" создано', 'yellow')]
-            except:
-                actions += [colored("Не удалось сохранить", 'yellow')]
-
-        elif action.split()[0] == 'load':
-            try:
-                save_name = "fast" if len(action.split()) == 1 else action.split()[1]
-                with open(f"saves/{save_name}.save", 'rb') as file:
-                    save_data = pickle.load(file)
-                    player = save_data['player']
-                    enemy_list = save_data['enemy_list']
-                    boss_flag = save_data['boss_flag']
-                actions = [colored(f'Сохранение "{save_name}" загружено', 'yellow')]
-            except:
-                actions += [colored("Не удалось загрузить сохранение", 'yellow')]
-
-        elif action == 'saves':
-            saves = ", ".join(map(lambda name: f'"{name[0:-5]}"', os.listdir("saves/")))
-            actions += [f"Список сохранений: {saves}"]
-
-        elif action.split()[0] in ['del', 'delete']:
-            try:
-                save_name = "fast" if len(action.split()) == 1 else action.split()[1]
-                os.remove(f"saves/{save_name}.save")
-                actions += [colored(f'Сохранение "{save_name}" удалено', 'yellow')]
-            except:
-                actions += [colored("Не удалось удалить сохранение", 'yellow')]
-
-        else:
-            actions += ["Some nonsense"]
-
-        if action in ['a', 'attack', 'd', 'defense']:
-            actions[-1] += enemy.action(player)
-        
-    if not (enemy_list or player.is_dead()):
+    def print_screen(self, stats, actions): # отрисовка текста на экране
         os.system('cls')
-        print(colored('\n\n\tYou win!', 'cyan'))
-    time.sleep(4)
-    return True
+        print(stats, '\n')
+        for action in actions:
+            print(action)
+
+    def main(self):
+        print('Your task is to defeat all monsters and stay alive, good luck!')
+        print('To attack press a, to defend press d\n')
+        self.player.name = input('Enter your name, hero: ')
+        self.enemy_list = self.gen_enemy_list(5, types=['Skeleton', 'Goblin', 'Orc'])
+        self.enemy_list += self.gen_enemy_list(5, types=['Goblin', 'Orc', 'Ogre'])
+        self.enemy_list += self.gen_enemy_list(5, types=['Orc', 'Ogre', 'Dragon'])
+        actions = []
+        self.boss_flag = True
+        
+        time.sleep(1)
+        os.system('cls')
+        while self.enemy_list and self.fight_run_flag:
+            self.enemy = self.enemy_list[0]
+            en_hl, en_mhl, en_pw = colored(str(self.enemy.health), 'red'),  colored(str(self.enemy.maxhealth), 'red'), colored(self.enemy.power, 'yellow')
+            pl_hl, pl_mhl, pl_pw = colored(str(self.player.health), 'green'), colored(str(self.player.maxhealth), 'green'), colored(self.player.power, 'yellow')
+            stats = f"{self.enemy.typ} {self.enemy.name} have {en_hl}/{en_mhl} health and {en_pw} power \nYou have {pl_hl}/{pl_mhl} health and {pl_pw} power"
+            self.print_screen(stats, actions)
+            if len(actions) > 16:
+                actions.pop(0)
+
+            if self.player.is_dead():
+                os.system('cls')
+                print(colored('\n\n\tYou died', 'red'))
+                time.sleep(4)
+                break
+            
+            if self.enemy.is_dead():
+                self.enemy.death()
+                self.enemy_list.pop(0)
+                
+                self.player.steal_stats(self.enemy)
+                
+                time.sleep(2)
+                actions = []
+                if not self.enemy_list:
+                    if self.boss_flag:
+                        self.boss_flag = False
+                        self.enemy = Enemy('Boss', f"Mirror of {self.player.name}")
+                        self.enemy_list += [self.enemy]
+                        self.enemy.health, self.enemy.maxhealth, self.enemy.power = self.player.health, self.player.maxhealth, self.player.power
+                continue
+            
+            action = Action(input('Your action: '))
+            message = action.do(self)
+            actions += message
+            
+        if not (self.enemy_list or self.player.is_dead()):
+            os.system('cls')
+            print(colored('\n\n\tYou win!', 'cyan'))
+            time.sleep(4)
+        return self.main_game_flag
 
 os.system('cls')
-while main():
+while Game().main():
     os.system('cls')
 os.system('cls')
